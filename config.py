f17e5c25b91b65d7025d63262ec729d6198f896e
Vixky <110277437+v1xky@users.noreply.github.com>
2022-12-15 11:53:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from os import getenv
 
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-MONGO_URL = getenv("MONGO_URL", "")
-BOT_ID = getenv("BOT_ID", "")#token first 10 digits
-AI_API_KEY = getenv("AI_API_KEY", "")
-AI_ID = int(getenv("AI_ID", ""))
+BOT_TOKEN = getenv("BOT_TOKEN", "2124064704:AAG-_oWI2-08SAgg1GbrU45OayzAHepIdAk")
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://vicky007:bickydana@cluster0.vmlde.mongodb.net/myFirstDatabase?")
+BOT_ID = getenv("BOT_ID", "2124064704")#token first 10 digits
+AI_API_KEY = getenv("AI_API_KEY", "S5Kv1YGgGtWy6Rys")
+AI_ID = int(getenv("AI_ID", "171162"))
